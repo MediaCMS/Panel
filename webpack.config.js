03df3ebd68e83c7858e9a7063b7a7a0ab3c0ce38
Artem Vysotsky <a.vysotsky@gmail.com>
2022-04-05 15:42:22 +0300

    add webpack/babel configs

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+"use strict"
+
+import webpack from "webpack";
+
+export default {
+  mode: "development",
+  entry: { index: "./src/index.js" },
+  output: { filename: "index.js" },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }, {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }, {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  experiments: {
+    topLevelAwait: true
+  }
+}
