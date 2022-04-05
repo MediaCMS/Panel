e148594b2aa9dfd76f339658bc7a76d137cc4dff
Artem Vysotsky <a.vysotsky@gmail.com>
2022-04-05 15:46:30 +0300

    add settings sample

---
+++ settings.sample.js
@@ -0,0 +1,12 @@
+'use strict'
+
+export default {
+    host: 'пк.медіа',
+    uri: '/ппі',
+    protocol: 'https',
+    ip: '127.0.0.1',
+    port: 8888,
+    path: 'media/panel',
+    db: 'mongodb://media:0123456789@localhost:27017/media',
+    key: '',
+}
