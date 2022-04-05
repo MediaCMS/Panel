518f8628f70439b49aa24e5f186d17d4d37f882b
Artem Vysotsky <a.vysotsky@gmail.com>
2022-04-05 20:14:14 +0300

    add routes

---
+++ routes.js
@@ -0,0 +1,19 @@
+"use strict"
+
+export default {
+    Article: { 
+        uri: "/статті",
+    }, 
+    Category: {
+        uri: "/категорії",
+    },
+    Tag: {
+        uri: "/мітки",
+    },
+    Page: {
+        uri: "/сторінки",
+    },
+    User: {
+        uri: "/користувачі",
+    }
+}
