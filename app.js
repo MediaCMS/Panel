2f6a24e2faaf4a4f7705760bac4ee85594f349f5
Artem Vysotsky <a.vysotsky@gmail.com>
2022-04-23 01:33:22 +0300

    add user login 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 import express from "express";
 import process from "process";
 import { MongoClient } from 'mongodb';
-import cookieSession from "cookie-session";
+import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 import routes from "./routes.js";
 import settings from "./settings.js";
@@ -17,7 +17,7 @@ const client = new MongoClient(settings.db);
 await client.connect();
 const db = client.db();
 const router = express.Router();
-const session = cookieSession(settings.session);
+//const session = cookieSession(settings.session);
 
 const controllers = {};
 for (const [controllerName, controller] of Object.entries(routes)) {
@@ -25,32 +25,34 @@ for (const [controllerName, controller] of Object.entries(routes)) {
         await import('./controllers/' + controllerName + '.js')
     ).default(db, controller);
 }
+const loginURI = settings.uri + routes.User.uri + routes.User.actions.login.uri;
 
-const allowed = [];
-allowed.push(settings.uri + routes.User.uri + routes.User.actions.login.uri);
-
-app.use(session);
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1);
-    session.secure = true;
+//    session.secure = true;
 }
 
 app.use(function (request, response, next) {
     //console.log(decodeURI(request.originalUrl));
     //console.log(decodeURI(request.path), request.params, request.query);
-    //console.log(request.headers);
-    if (typeof request.session.token === 'undefined') {
-        if (allowed.indexOf(decodeURI(request.path)) > -1) return next();
-        return response.sendStatus(401);
+    console.log(request.cookies);
+    if (!request.headers?.authorization) {
+        return response.status(401).end('Відсутня авторизація');
     }
-    // move to Layout
-    const user = jwt.verify(request.session.token, settings.key);
-    //console.log('user', user);
-    if (!user?._id) {
-        return response.sendStatus(401);
+    //console.log(request.headers.authorization);
+    if (decodeURI(request.path) === loginURI) return next();
+    const token = Buffer.from(request.headers.authorization.split(' ')[1], 'base64').toString();
+    //console.log('token', token);
+    try {
+        response.locals.user = jwt.verify(token, settings.key);
+        //console.log(response.locals.user);
+    } catch (error) {
+        //console.log(error.message);
+        return response.status(401).end(error.message);
     }
     next();
 });
