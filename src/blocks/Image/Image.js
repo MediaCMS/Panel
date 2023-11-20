a6e08180004161ae016bb75841f6cacb3e47bb62
Artem Vysotsky <a.vysotsky@gmail.com>
2023-11-20 21:13:04 +0200

    add upload image file validation

--- src/blocks/Image/Image.js
+++ src/blocks/Image/Image.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useOutletContext } from 'react-router-dom'
 import { Form } from 'react-bootstrap'
 import config from '../../config.js'
 
 export default function (props) {
 
+    const context = useOutletContext()
+
     const handleChange = event => {
-        console.log(event.target.files)
-        props.onChange(event.target.files[0])
+        if (!event.target.files.length) return
+        const file = event.target.files[0]
+        if (!(file.type in config.images.types)) {
+            event.target.value = null
+            context.setAlert(`Заборонений тип файлу зображення`)
+            return
+        }
+        if (file.size > config.images.size) {
+            event.target.value = null
+            context.setAlert(`Розмір файлу зображення більше дозволеного`)
+            return
+        }
+        props.onChange(file)
     }
 
     return (props?.id && props?.slug)
