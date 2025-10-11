6bf5cd4f9908305db9bbf82f1f39b3dd77479464
Artem Vysotsky <a.vysotsky@gmail.com>
2025-10-11 13:12:51 +0300

    Fix post deafult type

--- src/pages/Post/Editor.js
+++ src/pages/Post/Editor.js
@@ -12,6 +12,7 @@ const PostEditor = ({ id, show, onChange, onHide }) => {
     const [post, setPost] = useState({
         title: 'Заголовок', 
         date: new Date().toISOString(), 
+        type: '64be89ff69a893de210bd7d4',
         user: context.user._id,
         status: false
     })
